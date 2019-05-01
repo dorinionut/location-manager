2000a8f9a76b3209a7ad4fbc763e3d4dcde2f6f7
Dorin David <david.dorin@ymail.com>
2019-05-01 22:40:38 +0200

    Add E2E tests

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -10,12 +10,26 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to location-manager!');
+    expect(page.getTitleText()).toEqual('Location manager');
+  });
+
+  it('should search by ID', () => {
+    page.navigateTo();
+    page.searchForId();
+    expect(page.getNumberOfResults()).toBe(1);
+  });
+
+  it('should open the location edit form', () => {
+    page.navigateTo();
+    page.searchForId();
+    page.openLocationForm();
+    expect(page.getLocationForm()).toBeTruthy();
   });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+
     expect(logs).not.toContain(jasmine.objectContaining({
       level: logging.Level.SEVERE,
     } as logging.Entry));
--- client/e2e/src/app.po.ts
+++ client/e2e/src/app.po.ts
@@ -8,4 +8,21 @@ export class AppPage {
   getTitleText() {
     return element(by.css('app-root h1')).getText() as Promise<string>;
   }
+
+  getNumberOfResults() {
+    return element(by.css('p-table tbody tr')).all.length;
+  }
+
+  searchForId() {
+    element(by.id('filter-id')).sendKeys(65);
+  }
+
+  getLocationForm() {
+    return element(by.id('location-form'));
+  }
+
+  openLocationForm() {
+    element(by.css('p-table tbody tr')).click();
+  }
+
 }
